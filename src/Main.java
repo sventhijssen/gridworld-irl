5ccd788e78ca8ea446147b213d29012fb223e97c
Sven <thijssen_sven@hotmail.com>
2019-03-19 00:55:06 +0100

    distance exponential

--- src/Main.java
+++ src/Main.java
@@ -9,37 +9,37 @@ public class Main
         GridWorld gridWorld = new GridWorld(rows, columns);
 
         // Row 0
-        gridWorld.setFeature(0,0, new double[] {0, 4});
-        gridWorld.setFeature(0,1, new double[] {0, 5});
-        gridWorld.setFeature(0,2, new double[] {0, 6});
-        gridWorld.setFeature(0,3, new double[] {0, 6});
-        gridWorld.setFeature(0,4, new double[] {0, 8});
-        gridWorld.setFeature(0,5, new double[] {10, 9});
+        gridWorld.setFeature(0,0, new double[] {0, Math.exp(-1/4)});
+        gridWorld.setFeature(0,1, new double[] {0, Math.exp(-1/5)});
+        gridWorld.setFeature(0,2, new double[] {0, Math.exp(-1/6)});
+        gridWorld.setFeature(0,3, new double[] {0, Math.exp(-1/6)});
+        gridWorld.setFeature(0,4, new double[] {0, Math.exp(-1/8)});
+        gridWorld.setFeature(0,5, new double[] {1, Math.exp(-1/9)});
 
 
         // Row 1
-        gridWorld.setFeature(1,0, new double[] {0, 3});
-        gridWorld.setFeature(1,1, new double[] {0, 4});
-        gridWorld.setFeature(1,2, new double[] {0, 5});
-        gridWorld.setFeature(1,3, new double[] {0, 6});
-        gridWorld.setFeature(1,4, new double[] {0, 7});
-        gridWorld.setFeature(1,5, new double[] {0, 8});
+        gridWorld.setFeature(1,0, new double[] {0, Math.exp(-1/3)});
+        gridWorld.setFeature(1,1, new double[] {0, Math.exp(-1/4)});
+        gridWorld.setFeature(1,2, new double[] {0, Math.exp(-1/5)});
+        gridWorld.setFeature(1,3, new double[] {0, Math.exp(-1/6)});
+        gridWorld.setFeature(1,4, new double[] {0, Math.exp(-1/7)});
+        gridWorld.setFeature(1,5, new double[] {0, Math.exp(-1/8)});
 
         // Row 2
-        gridWorld.setFeature(2,0, new double[] {0, 2});
-        gridWorld.setFeature(2,1, new double[] {0, 3});
-        gridWorld.setFeature(2,2, new double[] {1, 4});
-        gridWorld.setFeature(2,3, new double[] {1, 5});
-        gridWorld.setFeature(2,4, new double[] {0, 6});
-        gridWorld.setFeature(2,5, new double[] {0, 7});
+        gridWorld.setFeature(2,0, new double[] {0, Math.exp(-1/2)});
+        gridWorld.setFeature(2,1, new double[] {0, Math.exp(-1/3)});
+        gridWorld.setFeature(2,2, new double[] {1, Math.exp(-1/4)});
+        gridWorld.setFeature(2,3, new double[] {1, Math.exp(-1/5)});
+        gridWorld.setFeature(2,4, new double[] {0, Math.exp(-1/6)});
+        gridWorld.setFeature(2,5, new double[] {0, Math.exp(-1/7)});
 
         // Row 3
-        gridWorld.setFeature(3,0, new double[] {0, 1});
-        gridWorld.setFeature(3,1, new double[] {0, 2});
-        gridWorld.setFeature(3,2, new double[] {0, 3});
-        gridWorld.setFeature(3,3, new double[] {0, 4});
-        gridWorld.setFeature(3,4, new double[] {0, 5});
-        gridWorld.setFeature(3,5, new double[] {0, 6});
+        gridWorld.setFeature(3,0, new double[] {0, Math.exp(-1)});
+        gridWorld.setFeature(3,1, new double[] {0, Math.exp(-1/2)});
+        gridWorld.setFeature(3,2, new double[] {0, Math.exp(-1/3)});
+        gridWorld.setFeature(3,3, new double[] {0, Math.exp(-1/4)});
+        gridWorld.setFeature(3,4, new double[] {0, Math.exp(-1/5)});
+        gridWorld.setFeature(3,5, new double[] {0, Math.exp(-1/6)});
 
         gridWorld.getStartPosition(0,0);
         gridWorld.setGoalPosition(0,5);
@@ -48,37 +48,37 @@ public class Main
         GridWorld testingWorld = new GridWorld(rows, columns);
 
         // Row 0
-        testingWorld.setFeature(0,0, new double[] {0, 1});
-        testingWorld.setFeature(0,1, new double[] {0, 2});
-        testingWorld.setFeature(0,2, new double[] {0, 3});
-        testingWorld.setFeature(0,3, new double[] {0, 4});
-        testingWorld.setFeature(0,4, new double[] {0, 5});
-        testingWorld.setFeature(0,5, new double[] {0, 6});
+        testingWorld.setFeature(0,0, new double[] {0, Math.exp(-1)});
+        testingWorld.setFeature(0,1, new double[] {0, Math.exp(-1/2)});
+        testingWorld.setFeature(0,2, new double[] {0, Math.exp(-1/3)});
+        testingWorld.setFeature(0,3, new double[] {0, Math.exp(-1/4)});
+        testingWorld.setFeature(0,4, new double[] {0, Math.exp(-1/5)});
+        testingWorld.setFeature(0,5, new double[] {0, Math.exp(-1/6)});
 
 
         // Row 1
-        testingWorld.setFeature(1,0, new double[] {0, 2});
-        testingWorld.setFeature(1,1, new double[] {0, 3});
-        testingWorld.setFeature(1,2, new double[] {0, 4});
-        testingWorld.setFeature(1,3, new double[] {0, 5});
-        testingWorld.setFeature(1,4, new double[] {0, 6});
-        testingWorld.setFeature(1,5, new double[] {0, 7});
+        testingWorld.setFeature(1,0, new double[] {0, Math.exp(-1/2)});
+        testingWorld.setFeature(1,1, new double[] {0, Math.exp(-1/3)});
+        testingWorld.setFeature(1,2, new double[] {0, Math.exp(-1/4)});
+        testingWorld.setFeature(1,3, new double[] {0, Math.exp(-1/5)});
+        testingWorld.setFeature(1,4, new double[] {0, Math.exp(-1/6)});
+        testingWorld.setFeature(1,5, new double[] {0, Math.exp(-1/7)});
 
         // Row 2
-        testingWorld.setFeature(2,0, new double[] {0, 3});
-        testingWorld.setFeature(2,1, new double[] {0, 4});
-        testingWorld.setFeature(2,2, new double[] {0, 5});
-        testingWorld.setFeature(2,3, new double[] {0, 6});
-        testingWorld.setFeature(2,4, new double[] {0, 7});
-        testingWorld.setFeature(2,5, new double[] {0, 8});
+        testingWorld.setFeature(2,0, new double[] {0, Math.exp(-1/3)});
+        testingWorld.setFeature(2,1, new double[] {0, Math.exp(-1/4)});
+        testingWorld.setFeature(2,2, new double[] {0, Math.exp(-1/5)});
+        testingWorld.setFeature(2,3, new double[] {0, Math.exp(-1/6)});
+        testingWorld.setFeature(2,4, new double[] {0, Math.exp(-1/7)});
+        testingWorld.setFeature(2,5, new double[] {0, Math.exp(-1/8)});
 
         // Row 3
-        testingWorld.setFeature(3,0, new double[] {1, 4});
-        testingWorld.setFeature(3,1, new double[] {0, 5});
-        testingWorld.setFeature(3,2, new double[] {0, 6});
-        testingWorld.setFeature(3,3, new double[] {0, 7});
-        testingWorld.setFeature(3,4, new double[] {0, 8});
-        testingWorld.setFeature(3,5, new double[] {10, 9});
+        testingWorld.setFeature(3,0, new double[] {1, Math.exp(-1/4)});
+        testingWorld.setFeature(3,1, new double[] {0, Math.exp(-1/5)});
+        testingWorld.setFeature(3,2, new double[] {0, Math.exp(-1/6)});
+        testingWorld.setFeature(3,3, new double[] {0, Math.exp(-1/7)});
+        testingWorld.setFeature(3,4, new double[] {0, Math.exp(-1/8)});
+        testingWorld.setFeature(3,5, new double[] {1, Math.exp(-1/9)});
 
         testingWorld.getStartPosition(0,0);
         testingWorld.setGoalPosition(3,5);
@@ -147,17 +147,16 @@ public class Main
         // Apply apprenticeship learning
         ApprenticeshipLearning apprenticeshipLearning = new ApprenticeshipLearning(gridWorld, expertPolicy);
         Vector weights = apprenticeshipLearning.solve();
-        System.out.println(weights);
 
         //TODO: Switch 1 and -5 to see heat map difference
         //Vector weights = new Vector(new double[] {1, 0});
 
-        //gridWorld.getRewardHeatMap(-1, weights);
         testingWorld.getRewardHeatMap(0, weights);
 
         // Apply Q-learning to example
         QLearning qLearning = new QLearning(testingWorld);
         qLearning.computeQTable(weights, 100);
         System.out.println(qLearning.toString());
+        System.out.println(weights);
     }
 }
